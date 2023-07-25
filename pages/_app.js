5f2a3924ca2213d71c5385f1892e984250a8acda
ericjiji3 <eric.ji3333@gmail.com>
2023-07-25 16:40:51 -0500

    added playsinLIne

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <video className="bgVideo" width="320" height="240" loop autoPlay muted>
+      <video className="bgVideo" width="320" height="240" loop={true} controls={false} autoPlay={true} muted playsInline>
         <source src="https://d1sxqb6wcb4nin.cloudfront.net/bg-video.mp4" type="video/mp4"/>
       </video>
       <Header/>
