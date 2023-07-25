529193497e0f733ebf3572c1b1b4a9d1c22256bd
ericjiji3 <eric.ji3333@gmail.com>
2023-07-25 16:53:50 -0500

    adding useeffect for autoplay vid

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import '@/styles/header.css'
 import Header from '../components/Header'
+import React, {useRef, useEffect} from "react"
 
 export default function App({ Component, pageProps }) {
+  const videoRef = useRef(undefined);
+  useEffect(()=>{
+    videoRef.current.defaultMuted = true;
+  })
   return (
     <div>
-      <video className="bgVideo" width="320" height="240" loop={true} controls={false} autoPlay={true} muted playsInline>
+      <video className="bgVideo" width="320" height="240" ref={videoRef} controls={false} loop autoPlay muted playsInline>
         <source src="https://d1sxqb6wcb4nin.cloudfront.net/bg-video.mp4" type="video/mp4"/>
       </video>
       <Header/>
